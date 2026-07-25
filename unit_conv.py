4fa5586553413dcc3af0900b9595bc714153205b
fullstack-architect-52 <15@felix-au.me>
2026-07-25 15:24:15 +0530

    Implement features in unit_conv.py - Version 8

--- unit_conv.py
+++ unit_conv.py
@@ -10,3 +10,6 @@ def kg_to_lbs(kg):
     return kg * 2.20462
 
 if __name__ == "__main__":
+    print("0 C in F:", celsius_to_fahrenheit(0))
+    print("10 km in miles:", km_to_miles(10))
+    print("5 kg in lbs:", kg_to_lbs(5))
